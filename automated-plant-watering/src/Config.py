9a93caa2571b55455f0588363fe8fe7e3bda4743
Neptun332 <patrykbednarz8@gmail.com>
2021-04-03 13:54:03 +0200

    created configuration loading

---
+++ automated-plant-watering/src/Config.py
@@ -0,0 +1,14 @@
+import os
+
+import ujson
+
+
+class Config:
+
+    def __init__(self, config_path):
+        print(os.listdir())
+        with open(config_path, "r") as file:
+            self.config = ujson.load(file)
+
+    def get_blynk_auth_token(self):
+        return self.config['blynk_auth_token']
--- automated-plant-watering/src/main.py
+++ automated-plant-watering/src/main.py
@@ -1,6 +1,9 @@
 import time
 
-from sensors.MoistureSensor import MoistureSensor
+from Config import Config
+from MoistureSensor import MoistureSensor
+
+CONFIG_PATH = "configuration\\config.json"
 
 if __name__ == "__main__":
     air_value = 3620
@@ -14,7 +17,10 @@ if __name__ == "__main__":
                                         air_value=air_value,
                                         water_value=water_value)
 
+    config = Config(CONFIG_PATH)
     while True:
+        print(config.get_blynk_auth_token())
+
         m_s_34_value = moisture_sensor_34.read_value()
 
         print("pin34: " + str(m_s_34_value))
